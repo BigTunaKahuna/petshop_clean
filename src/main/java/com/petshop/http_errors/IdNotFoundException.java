de264d218a6c55d10fe73830e96959eda2b11a7b
cmavrichi <cmavrichi@P5RO-PORT1190.pentalog.com>
2019-09-04 12:50:39 +0300

    Minor changes

--- src/main/java/com/petshop/http_errors/IdNotFoundException.java
+++ src/main/java/com/petshop/http_errors/IdNotFoundException.java
@@ -3,7 +3,7 @@ package com.petshop.http_errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Wrong id or it doesn't exist")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Wrong id or it doesn't exist!")
 public class IdNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 3001720877072869553L;
 }
