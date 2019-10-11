b8aad1531403948f780f15610ee893be233ade01
cmavrichi <cmavrichi@P5RO-PORT1190.pentalog.com>
2019-10-11 14:37:05 +0300

    Add tests for authority controller

--- src/test/java/com/petshop/controller_test/AuthorityControllerTest.java
+++ src/test/java/com/petshop/controller_test/AuthorityControllerTest.java
@@ -9,8 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,8 +50,9 @@ public class AuthorityControllerTest {
 		given(authorityService.saveAuthority(any(AuthorityDTO.class))).willReturn(authorityDTO);
 
 		this.mvc.perform(post("/role").content(mapper.writeValueAsString(authorityDTO))
-				.accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andDo(print())
+				.accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isCreated()).andExpect(jsonPath("$.role").value("ADMIN"));
+		verify(authorityService).saveAuthority(any(AuthorityDTO.class));
 	}
 
 	@Test
@@ -73,6 +76,84 @@ public class AuthorityControllerTest {
 		this.mvc.perform(get("/role/all").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andExpect(jsonPath("$.[0].role").value("USER"))
 				.andExpect(jsonPath("$.[1].role").value("ADMIN"));
+		verify(authorityService).getAllRoles();
 	}
+	
+	@Test
+	@WithMockUser(authorities = "ADMIN")
+	public void testChangeVetRole() throws Exception {
+		String success = "Role was successfully changed";
+		
+		doNothing().when(authorityService).changeRoleOfVet(anyLong(), any(Role.class), any(Role.class));
+		
+		this.mvc.perform(put("/role/change/vet/1/ADMIN/USER").content(mapper.writeValueAsString(success)).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").value(success));
+		verify(authorityService).changeRoleOfVet(anyLong(), any(Role.class), any(Role.class));
+	}
+	
+	@Test
+	@WithMockUser(authorities = "ADMIN")
+	public void testChangeCustomerRole() throws Exception {
+		String success = "Role was successfully changed";
+		
+		doNothing().when(authorityService).changeRoleOfCustomer(anyLong(), any(Role.class), any(Role.class));
+		
+		this.mvc.perform(put("/role/change/customer/1/USER/ADMIN").content(mapper.writeValueAsString(success)).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").value(success));
+		
+		verify(authorityService).changeRoleOfCustomer(anyLong(), any(Role.class), any(Role.class));
+	}
+	
+	@Test
+	@WithMockUser(authorities = "ADMIN")
+	public void testDeleteRoleForVet() throws Exception {
+		String success = "Role was successfully deleted";
+		
+		doNothing().when(authorityService).deleteRoleForVet(anyLong(), any(Role.class));
+		
+		this.mvc.perform(delete("/role/vet/1/ADMIN").content(mapper.writeValueAsString(success)).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").value(success));
+		verify(authorityService).deleteRoleForVet(anyLong(), any(Role.class));
+	}
+	
+	@Test
+	@WithMockUser(authorities = "ADMIN")
+	public void testDeleteRoleForCustomer() throws Exception {
+		String success = "Role was successfully deleted";
+		
+		doNothing().when(authorityService).deleteRoleForCustomer(anyLong(), any(Role.class));
+		
+		this.mvc.perform(delete("/role/customer/1/USER").content(mapper.writeValueAsString(success)).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").value(success));
+		verify(authorityService).deleteRoleForCustomer(anyLong(), any(Role.class));
+	}
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
